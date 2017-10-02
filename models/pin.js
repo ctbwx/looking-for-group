295a68be860308a1a10d9f012a9a38acfb5aa501
Charlie <ctbwilhite@gmail.com>
2017-10-02 12:43:56 -0500

    added pin.name to the database

--- models/pin.js
+++ models/pin.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 var pinSchema = new mongoose.Schema({
+    name: { type: String, required: false},
     title: { type: String, required: true },
     host_id: { type: String, required: true },
     description: {type: String, required: true },
